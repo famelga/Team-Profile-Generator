cc22ad7aaa0efc5b2af8782074c899cf8fd47e88
Fayven <fayven.amelga@gmail.com>
2023-01-25 19:31:52 -0500

    Link tests to lib js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,17 +1,1 @@
-class Employee{
-    constructor(){
-        this.fullName = fullName;
-        this.id = id;
-        this.email = email;
-    }
-}
-
-getName() {}
-
-getId() {}
-
-getEmail() {}
-
-getRole() {
-    return Employee;
-} 
+const Employee = require("../lib/Employee");
