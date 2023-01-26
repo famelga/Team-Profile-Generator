15810ea9135be4e413ae2816ff40756426b896e8
Fayven <fayven.amelga@gmail.com>
2023-01-25 19:57:16 -0500

    getName to Employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,15 @@
 const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+    it()
+
+    describe("getName", () => {
+        it("asks for the employee's name", () =>{
+            expect(new Employee("Fayven")).toBe(true);
+        });
+        it("Names less than 2 characters are not accepted", () => {
+            expect(new Employee("F")).toBe(false);
+        }) 
+    });
+   
+})
