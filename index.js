47b5cebd437d18e44ab573dfc1dda2d36fca133e
Fayven <fayven.amelga@gmail.com>
2023-01-26 01:52:55 -0500

    Add reset css

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
 
-const dist_dir = path.resolve(__dirname, "dist");
-const dist_path = path.join(dist_dir, "team.html");
-const render = require("./src/page-template");
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const path = require("path");
+
+// const dist_dir = path.resolve(__dirname, "dist");
+// const dist_path = path.join(dist_dir, "team.html");
+// const render = require("./src/page-template");
