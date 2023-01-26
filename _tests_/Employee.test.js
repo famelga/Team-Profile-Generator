83d8f34f3540fb29e85bf52bdef40490303e5021
Fayven <fayven.amelga@gmail.com>
2023-01-25 21:41:36 -0500

    Change const testEmployee syntax on Employee.test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,52 +1,57 @@
 const Employee = require("../lib/Employee");
 
-// describe("Employee class", () => {
-    test("Can create an email", () => {
-        const e = new Employee();
-        expect(typeof (e)).toBe("object");
+describe("Employee class", () => {
+
+    describe("constructor", () => {
+    it("Can create an email", () => {
+        const testEmployee = new Employee("Fayven", 30, "fam@me.me");
+        expect(testEmployee.name).toEqual("Fayven");
+        expect(testEmployee.id).toEqual(30);
+        expect(testEmployee.email).toEqual("fam@me.me");
     })
 
-    test("Can set name via constructor", () => {
-        const name = "fayven";
-        const e = new Employee(name);
-        expect(e.name).toBe(name);
-    })
+    // it("Can set name via constructor", () => {
+    //     const name = "fayven";
+    //     const e = new Employee(name);
+    //     expect(e.name).toBe(name);
+    // })
 
-    test("Can set id via constructor", () => {
-        const id = 30;
-        const e = new Employee(id);
-        expect(e.id).toBe(id);
-    })
+    // it("Can set id via constructor", () => {
+    //     const id = 30;
+    //     const e = new Employee(id);
+    //     expect(e.name).toBe(id);
+    // })
 
-    test("Can set email via constructor", () => {
-        const email = "fam@me.com";
-        const e = new Employee(email);
-        expect(e.email).toBe(email);
-    })
+    // it("Can set email via constructor", () => {
+    //     const email = "fam@me.com";
+    //     const e = new Employee(email);
+    //     expect(e.email).toBe(email);
+    // })
 
-    // describe("Can get name via getName", () => {
-        test("returns employee's name", () =>{
-            const name = new Employee("Fayven");
-            expect(name.getName()).toBe("Fayven");
+    describe("Can get name via getName", () => {
+        it("returns employee's name", () =>{
+            const testEmployee = new Employee("Fayven");
+            expect(testEmployee.getName()).toBe("Fayven");
         });
-    // });
-    // describe("Can get id via getId", () => {
-        test("returns employee's id", () =>{
-            const id = new Employee("Fayven", 30);
-            expect(id.getId()).toBe(30);
+    });
+    describe("Can get id via getId", () => {
+        it("returns employee's id", () =>{
+            const testEmployee = new Employee("Fayven", 30);
+            expect(testEmployee.getId()).toBe(30);
         });
-    // });
-    // describe("Can get email via getEmail", () => {
-        test("returns employee's email", () =>{
-            const email = new Employee("Fayven", 30, "fam@me.com");
-            expect(email.getEmail()).toBe("fam@me.com");
+    });
+    describe("Can get email via getEmail", () => {
+        it("returns employee's email", () =>{
+            const testEmployee = new Employee("Fayven", 30, "fam@me.com");
+            expect(testEmployee.getEmail()).toBe("fam@me.com");
         });
-    // });
-    // describe("Can get role via getRole", () => {
-        test("returns employee's role", () =>{
-            const role = new Employee("Fayven", 30, "fam@me.com");
-            expect(role.getRole()).toBe("Employee");
+    });
+    describe("Can get role via getRole", () => {
+        it("returns employee's role", () =>{
+            const testEmployee = new Employee("Fayven", 30, "fam@me.com");
+            expect(testEmployee.getRole()).toBe("Employee");
         });
-    // });
+    });
+});
    
-// })
+});
