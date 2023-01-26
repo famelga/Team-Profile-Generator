db66c74ae60853bb7f8ac372fb9b8d2de7bf6dfe
Fayven <fayven.amelga@gmail.com>
2023-01-25 20:48:41 -0500

    Add Employee.test code

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,15 +1,52 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee class", () => {
-    it()
+// describe("Employee class", () => {
+    test("Can create an email", () => {
+        const e = new Employee();
+        expect(typeof (e)).toBe("object");
+    })
 
-    describe("getName", () => {
-        it("asks for the employee's name", () =>{
-            expect(new Employee("Fayven")).toBe(true);
+    test("Can set name via constructor", () => {
+        const name = "fayven";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    })
+
+    test("Can set id via constructor", () => {
+        const id = 30;
+        const e = new Employee(id);
+        expect(e.id).toBe(id);
+    })
+
+    test("Can set email via constructor", () => {
+        const email = "fam@me.com";
+        const e = new Employee(email);
+        expect(e.email).toBe(email);
+    })
+
+    // describe("Can get name via getName", () => {
+        test("returns employee's name", () =>{
+            const name = new Employee("Fayven");
+            expect(name.getName()).toBe("Fayven");
         });
-        it("Names less than 2 characters are not accepted", () => {
-            expect(new Employee("F")).toBe(false);
-        }) 
-    });
+    // });
+    // describe("Can get id via getId", () => {
+        test("returns employee's id", () =>{
+            const id = new Employee("Fayven", 30);
+            expect(id.getId()).toBe(30);
+        });
+    // });
+    // describe("Can get email via getEmail", () => {
+        test("returns employee's email", () =>{
+            const email = new Employee("Fayven", 30, "fam@me.com");
+            expect(email.getEmail()).toBe("fam@me.com");
+        });
+    // });
+    // describe("Can get role via getRole", () => {
+        test("returns employee's role", () =>{
+            const role = new Employee("Fayven", 30, "fam@me.com");
+            expect(role.getRole()).toBe("Employee");
+        });
+    // });
    
-})
+// })
