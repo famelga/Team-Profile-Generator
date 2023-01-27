fc9a7d8bb8d16b5083414e8f45e0a2c99f555067
Fayven <fayven.amelga@gmail.com>
2023-01-27 00:58:10 -0500

    Add manager placegolders to var index

--- index.js
+++ index.js
@@ -17,32 +17,35 @@ function managerCard() {
 inquirer
 .prompt([
     {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role?', 
+        choices: ['Manager'],
+    },
+    {
         name: 'name',
         message: 'Please enter your full name.', 
-        // validate: ,
     },
     {
         type: 'number',
         name: 'id',
         message: 'Please enter your id.', 
-        // validate ,
     },
     {
         name: 'email',
         message: 'Please enter your email address.', 
-        // validate ,
     },
     {
         type: 'number',
         name: 'office',
         message: 'Please enter your office number.', 
-        // validate ,
     },
 ])
 .then((response) => {
+    allEmployees.push(response);
 
-    const manager = new Manager(response.name, response.id, response.email, response.office);
-    console.log(manager);
+    // const manager = new Manager(response.name, response.id, response.email, response.office);
+    // console.log(manager);
     employeeCard();
 });
 };
@@ -55,23 +58,19 @@ function employeeCard(){
             name: 'role',
             message: 'What role would you like to add?', 
             choices: ['Intern', 'Engineer'],
-            // validate ,
         },
         {
             name: 'name',
             message: 'Please enter employee full name.', 
-            // validate: ,
         },
         {
             type: 'number',
             name: 'id',
             message: 'Please enter employee id.', 
-            // validate ,
         },
         {
             name: 'email',
             message: 'Please enter employee email address.', 
-            // validate ,
         },
         {
             name: 'school',
@@ -82,113 +81,94 @@ function employeeCard(){
             name: 'github',
             message: 'Please enter employee github username.', 
             when: (response) => response.role === "Engineer",
-            // validate ,
         },
         {
             type: 'confirm',
             name: 'add',
             message: 'Would you like to add another employee?', 
-
-            // validate:  ,
         },
-        // {
-        //     name: ,
-        //     message , 
-        //     validate ,
-        // },
+
     ])
     .then((response) => {
 
-                // for (i = 0; i <response.length; i++) {
             allEmployees.push(response);
-        // }
 
         if(response.add === true) {
-            // allEmployees.push(response);
             employeeCard();
         };
 
-
+        myTeam();
 
         // const engineer = new Engineer(response.role, response.name, response.id, response.email, response.office);
         // const intern = new Intern(response.role, response.name, response.id, response.email, response.office);
 
-        // console.log(engineer);
-        // console.log(intern);
-        console.log(response);
+        console.log(allEmployees);
 
     });
     };
 
     managerCard();
-    // employeeCard();
 
 
+function myTeam() {
 
-
-
-
-
-
-
-// var index = `<!DOCTYPE html>
-// //         <html lang="en">
+var index = `<!DOCTYPE html>
+        <html lang="en">
         
-// //         <head>
-// //             <meta charset="UTF-8">
-// //             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-// //             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-// //             <title>Document</title>
-// //             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
-// //         </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+        </head>
         
-// //         <body>
-// //             <header class="column has-background-primary p-4 has-text-centered">My Team</header>
-// //             <main class="container">
-// //                 <div class="card">
-// //                     <div class="card-header">
-// //                     <div class="card-header-title is-center ">${response.name}</div>
-// //                     <div class="card-header-title is-center">${response.role}</div>
-// //                 </div>
-// //                     <div class="card-content" id="id"></div>
-// //                     <div class="card-content" id="email"></div>
-// //                     <div class="card-content" id="office-number"></div>
-// //                     <div class="card-content"></div>
-// //                     </div>
-// //                 </div>
-// //                 <div class="card">
-// //                     <div class="card-header">
-// //                     <div class="card-header-title is-center "></div>
-// //                     <div class="card-header-title is-center"></div>
-// //                 </div>
-// //                     <div class="card-content" id="id"></div>
-// //                     <div class="card-content" id="email"></div>
-// //                     <div class="card-content" id="github"></div>
-// //                     <div class="card-content"></div>
-// //                     </div>
-// //                 </div>
-// //                 <div class="card">
-// //                     <div class="card-header">
-// //                     <div class="card-header-title is-center "></div>
-// //                     <div class="card-header-title is-center"></div>
-// //                 </div>
-// //                     <div class="card-content" id="id"></div>
-// //                     <div class="card-content" id="email"></div>
-// //                     <div class="card-content" id="school"></div>
-// //                     <div class="card-content"></div>
-// //                     </div>
-// //                 </div>
-// //             </main>
-// //         </body>
+        <body>
+            <header class="column has-background-primary p-4 has-text-centered">My Team</header>
+            <main class="container">
+                <div class="card">
+                    <div class="card-header">
+                    <div class="card-header-title is-center ">${allEmployees[0].name}</div>
+                    <div class="card-header-title is-center">${allEmployees[0].role}</div>
+                </div>
+                    <div class="card-content" id="id">${allEmployees[0].id}</div>
+                    <div class="card-content" id="email">${allEmployees[0].email}</div>
+                    <div class="card-content" id="office-number">${allEmployees[0].office}</div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                    <div class="card-header-title is-center "></div>
+                    <div class="card-header-title is-center"></div>
+                </div>
+                    <div class="card-content" id="id"></div>
+                    <div class="card-content" id="email"></div>
+                    <div class="card-content" id="github"></div>
+                    <div class="card-content"></div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                    <div class="card-header-title is-center "></div>
+                    <div class="card-header-title is-center"></div>
+                </div>
+                    <div class="card-content" id="id"></div>
+                    <div class="card-content" id="email"></div>
+                    <div class="card-content" id="school"></div>
+                    <div class="card-content"></div>
+                    </div>
+                </div>
+            </main>
+        </body>
         
-// //         </html>`
+        </html>`
     
-// //         // fs.writeFile('index.html', index, (err) => {
-// //         //     if (err) throw err;
-// //         //     console.log(index)
-// //         // })
+        fs.writeFile('index.html', index, (err) => {
+            if (err) throw err;
+            console.log(index)
+        })
     
-
+}
 
 
 
